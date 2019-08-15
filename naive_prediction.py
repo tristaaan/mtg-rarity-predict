9680b8733ccb544d9b9450a782c0ca50be7f6872
Tristan <thrawst88@gmail.com>
2019-08-15 12:44:00 -0600

    class balancing

--- naive_prediction.py
+++ naive_prediction.py
@@ -1,13 +1,16 @@
 import argparse
 import itertools
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
 
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.calibration import CalibratedClassifierCV
 from sklearn.svm import LinearSVC
+
 
 def get_train_test_split(df):
     '''
@@ -19,7 +22,8 @@ def get_train_test_split(df):
     df = df.sample(frac=1, random_state=rs)
 
     # Select same number of samples per class for train set, remaining go to test set
-    num_of_train_inputs = int(rows * 0.8)
+    # num_of_train_inputs = int(rows * 0.8)
+    num_of_train_inputs = int(get_min_rarity_count(cards) * 0.8)
     type_cmc = ['type', 'cmc', 'legendary']
     full_inputs = ['type', 'C', 'R', 'U', 'B', 'G', 'W', 'X', 'S',  \
                    'B/G', 'B/R', 'G/U', 'G/W', 'R/G', 'R/W', 'U/B', \
@@ -51,28 +55,33 @@ def get_train_test_split(df):
     return (train_values, train_labels, test_values, test_labels)
 
 
-def plot_confusion_matrix(cm, labels):
+def get_min_rarity_count(cards):
+    rarities = np.unique(cards.loc[:,['rarity']].values)
+    counts = cards['rarity'].value_counts(dropna=False)
+    return min(counts)
+
+
+def plot_confusion_matrix(cm, labels, method):
     '''
     Plot confusion matrix of the specified accuracies and labels
     '''
     accuracy = np.trace(cm) / float(np.sum(cm))
     misclass = 1 - accuracy
 
-    cmap = plt.get_cmap('Blues')
-
+    # cmap = plt.get_cmap('Blues')
     fig = plt.figure(figsize=(6,6))
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title('Confusion matrix')
-    # plt.colorbar()
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis] # normalize
+    plt.imshow(cm, interpolation='nearest', cmap='Blues',
+               norm=colors.Normalize(vmin=0.0, vmax=1.0))
+    plt.title('Confusion matrix - %s' % method.upper())
+    plt.colorbar()
 
     # Draw ticks
     tick_marks = np.arange(len(labels))
     plt.xticks(tick_marks, labels, rotation=45)
     plt.yticks(tick_marks, labels)
 
-    # Normalize
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    # Label values
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
         plt.text(j, i, "{:0.2f}".format(cm[i, j]),
                  horizontalalignment="center",
@@ -81,8 +90,8 @@ def plot_confusion_matrix(cm, labels):
     plt.tight_layout()
     plt.ylabel('True rarity')
     plt.xlabel('Predicted rarity\naccuracy={:0.2f}; misclass={:0.2f}'.format(accuracy, misclass))
-    fig.subplots_adjust(bottom=0.2)
-    fig.savefig('naive_confusion_matrix.png')
+    fig.subplots_adjust(bottom=0.1, left=0.2)
+    fig.savefig('naive_confusion_matrix-%s.png' % method)
 
 
 def mlp_classification(train_values, train_labels):
@@ -92,11 +101,13 @@ def mlp_classification(train_values, train_labels):
     mlp.fit(train_values, train_labels)
     return mlp
 
+
 def svm_classification(train_values, train_labels):
     clf = LinearSVC()
     svm = CalibratedClassifierCV(clf)
     svm.fit(train_values, train_labels)
     return svm
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Naive card predicion')
@@ -104,11 +115,12 @@ if __name__ == '__main__':
                         default='mlp')
     args = parser.parse_args()
     kw = vars(args)
+    method = kw['classifier']
 
     cards = pd.read_csv('processed_sets.csv', sep='\t')
     train_values, train_labels, test_values, test_labels = \
         get_train_test_split(cards)
-    if kw['classifier'] == 'mlp':
+    if method == 'mlp':
         print('classifying with MLP...')
         model = mlp_classification(train_values, train_labels)
     else:
@@ -116,9 +128,10 @@ if __name__ == '__main__':
         model = svm_classification(train_values, train_labels)
 
     predicted_labels = model.predict(test_values)
+    sorted_labels = ['common', 'uncommon', 'rare', 'mythic']
     acc = accuracy_score(test_labels, predicted_labels)
-    cm = confusion_matrix(test_labels, predicted_labels)
+    cm = confusion_matrix(test_labels, predicted_labels, sorted_labels)
 
     labels = np.unique(cards.loc[:,['rarity']].values).tolist()
-    plot_confusion_matrix(cm, labels)
-    print('accuracy with %s: %0.2f' % (kw['classifier'].upper(), acc))
+    plot_confusion_matrix(cm, sorted_labels, method)
+    print('accuracy with %s: %0.2f' % (method.upper(), acc))
