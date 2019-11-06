42d37310765b457c1e2b130ec81b65f712e94d7e
Tristan <thrawst88@gmail.com>
2019-11-05 17:12:25 -0700

    documentation, cleanup

--- naive_prediction.py
+++ naive_prediction.py
@@ -24,6 +24,9 @@ def get_train_test_split(df):
     # Select same number of samples per class for train set, remaining go to test set
     # num_of_train_inputs = int(rows * 0.8)
     num_of_train_inputs = int(get_min_rarity_count(cards) * 0.8)
+    print(num_of_train_inputs)
+
+    # inputs
     type_cmc = ['type', 'cmc', 'legendary']
     full_inputs = ['type', 'C', 'R', 'U', 'B', 'G', 'W', 'X',  \
                    'B/G', 'B/R', 'G/U', 'G/W', 'R/G', 'R/W', 'U/B', \
@@ -31,6 +34,7 @@ def get_train_test_split(df):
     inputs = full_inputs
     train_df, test_df = (pd.DataFrame(columns=inputs), pd.DataFrame(columns=inputs))
     rarities = np.unique(df.loc[:,['rarity']].values)
+    print(rarities)
     for rarity in rarities:
         train_df = train_df.append(df[:num_of_train_inputs].loc[df['rarity'] == rarity, inputs+['rarity']], ignore_index=True)
         test_df  =  test_df.append(df[num_of_train_inputs:].loc[df['rarity'] == rarity, inputs+['rarity']], ignore_index=True)
@@ -39,25 +43,30 @@ def get_train_test_split(df):
     train_df = train_df.sample(frac=1, random_state=rs)
     test_df  = test_df.sample(frac=1, random_state=rs)
 
-    types = np.unique(df.loc[:,['type']].values).tolist()
+    # encode spell type as integer.
+    spell_types = np.unique(df.loc[:,['type']].values).tolist()
 
-    # Split train and test datasets into labels/features
-    train_values = train_df[inputs].values
-    for r in train_values:
-        r[0] = types.index(r[0])
-    train_labels = train_df['rarity'].values.ravel()
-
-    test_values = test_df[inputs].values
-    for r in test_values:
-        r[0] = types.index(r[0])
-    test_labels = test_df['rarity'].values.ravel()
+    # Split train and test datasets into features/labels
+    train_values, train_labels = split(train_df, inputs, spell_types)
+    test_values, test_labels = split(test_df, inputs, spell_types)
 
     return (train_values, train_labels, test_values, test_labels)
+
+
+def split(df, columns, spell_types):
+    values = df[columns].values
+    # convert spell type into numerical value
+    for r in values:
+        # len(spell_types) // 2 to normalize spell type
+        r[0] = spell_types.index(r[0]) - len(spell_types)//2
+    labels = df['rarity'].values.ravel()
+    return (values, labels)
 
 
 def get_min_rarity_count(cards):
     rarities = np.unique(cards.loc[:,['rarity']].values)
     counts = cards['rarity'].value_counts(dropna=False)
+    print(counts)
     return min(counts)
 
 
@@ -127,6 +136,7 @@ if __name__ == '__main__':
         print('classifying with SVM...')
         model = svm_classification(train_values, train_labels)
 
+
     predicted_labels = model.predict(test_values)
     sorted_labels = ['common', 'uncommon', 'rare', 'mythic']
     acc = accuracy_score(test_labels, predicted_labels)
@@ -135,3 +145,5 @@ if __name__ == '__main__':
     labels = np.unique(cards.loc[:,['rarity']].values).tolist()
     plot_confusion_matrix(cm, sorted_labels, method)
     print('accuracy with %s: %0.2f' % (method.upper(), acc))
+    # krasis = np.array([[2, 4, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.0]])
+    # print(model.predict_proba(krasis))
